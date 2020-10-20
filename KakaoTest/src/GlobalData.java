dd0e0d2bb83837e1030585da499c3927cbcd3400
dlgodrk <60539784+dlgodrk@users.noreply.github.com>
2020-10-20 20:55:55 +0900

    remove file

--- KakaoTest/src/GlobalData.java
+++
@@ -1,9 +0,0 @@
-
-public class GlobalData {
-	public final static String X_AUTH_TOKEN = "a04e831f00969b036f9ae75c39e0c91a";
-	public final static String BASE_URL = "https://pegkq2svv6.execute-api.ap-northeast-2.amazonaws.com/prod/users";
-	public final static String POST_START = "/start";
-	public final static String GET_LOCATIONS = "/locations";
-	public final static String GET_TRUCKS = "/trucks";
-	public final static String PUT_SIMULATE = "/simulate";
-}
